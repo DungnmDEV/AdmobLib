e3c7f6cc857868aa2c1b301ccdbd778ae257f1cb
Nguyen Dung <nguyendungzck@gmail.com>
2024-04-15 17:43:42 +0700

    Initial commit

---
+++ AdmobLib/src/main/java/com/dungvnhh98/percas/studio/admoblib/Example.kt
@@ -0,0 +1,10 @@
+package com.dungvnhh98.percas.studio.admoblib
+
+import android.content.Context
+import android.widget.Toast
+
+object Example {
+    fun showMes(context: Context){
+        Toast.makeText(context, "It's oke!", Toast.LENGTH_LONG).show()
+    }
+}
